a957a7a6bc77e2e4eeca811263f51bd07f892f8a
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-07 23:07:27 +0200

    added Strip and Collectible types

---
+++ src/frontend/src/types/Collectible.ts
@@ -0,0 +1,14 @@
+export type Collectible = {
+    idArtikla: number;
+    naziv: string;
+    opis: string;
+    dostupnaKolicina: number;
+    cijena: number;
+    PDV: number;
+    izdavac: string;
+    izdanje: string;
+    masa: number;
+    pakiranje: number;
+    dimenzije: string;
+    proizvodac: string;
+};
---
+++ src/frontend/src/types/Strip.ts
@@ -0,0 +1,13 @@
+export type Strip = {
+    idArtikla: number;
+    naziv: string;
+    opis: string;
+    dostupnaKolicina: number;
+    cijena: number;
+    PDV: number;
+    izdavac: string;
+    izdanje: string;
+    brojStranica: number;
+    jezik: string;
+    autor: string;
+};
