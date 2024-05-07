77c01a4fc8c267ccb23ab812a4cea16f992c0a00
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-07 19:38:34 +0200

    added NarudzbaView.vue

---
+++ src/frontend/src/views/NarudzbaView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>The parameter value is: {{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        },
+    },
+};
+</script>
