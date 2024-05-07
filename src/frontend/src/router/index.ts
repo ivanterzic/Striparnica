4324cd5bae40884b32908314bd24c999836a5a72
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-07 19:37:06 +0200

    added /narudzbe, /narudzba and catchall routes

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -1,25 +1,30 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import NarudzbeView from "../views/NarudzbeView.vue";
+import NarudzbaView from "../views/NarudzbaView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    {
+        path: "/narudzbe",
+        name: "narudzbe",
+        component: NarudzbeView,
+    },
+    {
+        path: "/narudzba/:id",
+        name: "narudzba",
+        component: NarudzbaView,
+        props: true,
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "notFound",
+        component: NotFoundView,
+    },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+    history: createWebHashHistory(),
+    routes,
+});
 
-export default router
+export default router;
