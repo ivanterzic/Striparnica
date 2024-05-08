94a33b20a1e884ef4fc3e068a9a645823644601b
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-08 23:20:38 +0200

    added cors

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,26 +1,29 @@
+const express = require("express");
+const { Application } = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const express = require('express');
-const { Application } = require('express');
-const bodyParser = require('body-parser');
-
-import narudzbaRoutes from './routes/narudzba.routes';
-import dobavljacRoutes from './routes/dobavljac.routes';
-import artikalRoutes from './routes/artikal.routes';
-import zaposlenikRoutes from './routes/zaposlenik.routes';
+import narudzbaRoutes from "./routes/narudzba.routes";
+import dobavljacRoutes from "./routes/dobavljac.routes";
+import artikalRoutes from "./routes/artikal.routes";
+import zaposlenikRoutes from "./routes/zaposlenik.routes";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 //body parser
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
 app.use(bodyParser.json());
 
-app.use('/dobavljaci', dobavljacRoutes);
-app.use('/narudzbe', narudzbaRoutes);
-app.use('/artikli', artikalRoutes);
-app.use('/zaposlenici', zaposlenikRoutes);
+app.use("/dobavljaci", dobavljacRoutes);
+app.use("/narudzbe", narudzbaRoutes);
+app.use("/artikli", artikalRoutes);
+app.use("/zaposlenici", zaposlenikRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
