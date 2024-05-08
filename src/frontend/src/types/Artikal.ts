17ea7aa5815141edfa3e12fe8d0f58e48086bb6c
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-08 12:59:07 +0200

    added Artikal type

---
+++ src/frontend/src/types/Artikal.ts
@@ -0,0 +1,10 @@
+export type Artikal = {
+    idArtikla: number;
+    naziv: string;
+    opis: string;
+    dostupnaKolicina: number;
+    cijena: number;
+    PDV: number;
+    izdavac: string;
+    izdanje: string;
+};
