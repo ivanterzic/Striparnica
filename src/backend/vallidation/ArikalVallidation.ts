a3c7b1b7ed3588b5ab5e0333e1474eabb7558874
ivanterzic <ivan.terzic@fer.hr>
2024-05-11 10:11:52 +0200

    date and error messages correction

--- src/backend/vallidation/ArikalVallidation.ts
+++ src/backend/vallidation/ArikalVallidation.ts
@@ -8,11 +8,11 @@ export async function artiklVallidation(req: Request, res: Response, next: Funct
     let idNarudzba = (await Narudzba.dohvatiSveNarudzbe() as { idnarudzbe: number }[]).map((n) => n.idnarudzbe.toString());
     let id = (req.params.id);
     if (!req.body.stavkenarudzbe) {
-        res.status(400).json({ error: 'Nedostaju stavke narudzbe' });
+        res.status(400).json({ error: 'Nedostaju stavke narudžbe.' });
         return;
     }
     if (!id || !idNarudzba.includes(id)) {
-        res.status(400).json({ error: 'Id narudzbe nije medju narudzbama' });
+        res.status(400).json({ error: 'Id naružbe nije validan.' });
         return;
     }
     let stavke;
@@ -20,24 +20,24 @@ export async function artiklVallidation(req: Request, res: Response, next: Funct
         stavke = typeof req.body.stavkenarudzbe === 'string' ? JSON.parse(req.body.stavkenarudzbe) : req.body.stavkenarudzbe;
     }
     catch (err) {
-        res.status(400).json({ error: 'Stavke narudzbe nisu validne' });
+        res.status(400).json({ error: 'Stavke narudžbe nisu u validnom formatu.' });
         return;
     }
     for (let stavka of stavke) {
-        if (!stavka.idartikla || !stavka.kolicina) {
-            res.status(400).json({ error: 'Nedostaju obavezna polja u stavci narudzbe' });
+        if (!stavka.idartikla || stavka.kolicina == undefined || stavka.kolicina == null) {
+            res.status(400).json({ error: 'Nedostaju obavezna polja u stavci narudžbe.' });
             return;
         }
         if (typeof stavka.kolicina != 'number') {
-            res.status(400).json({ error: 'Kolicina mora biti broj' });
+            res.status(400).json({ error: 'Količina mora biti broj.' });
             return;
         }
         if (stavka.kolicina <= 0) {
-            res.status(400).json({ error: 'Kolicina mora biti pozitivan broj' });
+            res.status(400).json({ error: 'Količina mora biti veća od 0.' });
             return;
         }
         if (!artikli.includes(stavka.idartikla)) {
-            res.status(400).json({ error: 'Id artikla nije medju artiklima' });
+            res.status(400).json({ error: 'Id artikla nije među validnim artiklima.' });
             return;
         }
     }
--- src/backend/vallidation/NarudzbaVallidation.ts
+++ src/backend/vallidation/NarudzbaVallidation.ts
@@ -10,24 +10,28 @@ export async function narudzbaVallidation(req: Request, res: Response, next: Fun
     let statusi = await Narudzba.dohvatiSveStatuseNarudzbi();
 
     if (!req.body.datumstvaranja || !req.body.status || !req.body.mbrreferenta || !req.body.iddobavljaca) {
-        res.status(400).json({ error: 'Nedostaju obavezna polja' });
+        res.status(400).json({ error: 'Nedostaju obavezna polja.' });
         return;
     }
     let iddobavljaca = parseInt(req.body.iddobavljaca);
     let datumstvaranja = new Date(req.body.datumstvaranja);
     let datumzaprimanja = new Date(req.body.datumzaprimanja);
     if (isNaN(datumstvaranja.getTime())) {
-        res.status(400).json({ error: 'Datum stvaranja nije validan datum' });
+        res.status(400).json({ error: 'Datum stvaranja nije validan datum.' });
+        return;
+    }
+    if (datumstvaranja.getTime() > Date.now()) {
+        res.status(400).json({ error: 'Datum stvaranja ne može biti u budućnosti.' });
         return;
     }
     if (!req.body.datumzaprimanja || req.body.datumzaprimanja === "" || req.body.datumzaprimanja === null) {
         req.body.datumzaprimanja = null;
     } else if (isNaN(datumzaprimanja.getTime())) {
-        res.status(400).json({ error: 'Datum zaprimanja nije validan datum' });
+        res.status(400).json({ error: 'Datum zaprimanja nije validan datum.' });
         return;
     }
     else if (datumzaprimanja.getTime() < datumstvaranja.getTime()) {
-        res.status(400).json({ error: 'Datum zaprimanja mora biti nakon datuma stvaranja' });
+        res.status(400).json({ error: 'Datum zaprimanja mora biti nakon datuma stvaranja.' });
         return;
     }
     if (req.body.datumzaprimanja){
@@ -35,15 +39,15 @@ export async function narudzbaVallidation(req: Request, res: Response, next: Fun
     }
     req.body.datumstvaranja += "T00:00:00.000Z";
     if (!referenti.includes(req.body.mbrreferenta)) {
-        res.status(400).json({ error: 'Mbr referenta nije medju referentima nabave' });
+        res.status(400).json({ error: 'Mbr referenta nije među referentima nabave.' });
         return;
     }
     if (!dobavljaci.includes(iddobavljaca)) {
-        res.status(400).json({ error: 'Id dobavljaca nije medju dobavljacima' });
+        res.status(400).json({ error: 'Id dobavljača nije među validnim dobavljačima.' });
         return;
     }
     if (!statusi.includes(req.body.status)) {
-        res.status(400).json({ error: 'Status nije medju validnim statusima narudzbe' });
+        res.status(400).json({ error: 'Status nije među validnim statusima narudžbe.' });
         return;
     }
 
