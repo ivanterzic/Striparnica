b1e58f523eb532bf839d40a3a12404b966448bbe
Vjekoslav Radošević <vr53601@fer.hr>
2024-05-26 19:46:51 +0200

    deleted commented code from backend tests

--- src/backend/__tests__/integration_tests/artikalController.test.ts
+++ src/backend/__tests__/integration_tests/artikalController.test.ts
@@ -1,87 +1,7 @@
-import { Artikal } from '../../models/artikal';
-import { ArtikalController } from '../../controllers/artikalController';
+import { Artikal } from "../../models/artikal";
+import { ArtikalController } from "../../controllers/artikalController";
 
-/*
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-
-const artikalClass =
-    class Artikal {
-        idartikla: number;
-        naziv: string;
-        opis: string;
-        dostupnakolicina: number;
-        cijena: number;
-        pdv: number;
-        izdavac: string;
-        izdanje: string;
-
-        private constructor(id: number, naziv: string, opis: string, dostupnakolicina: number, cijena: number, pdv: number, izdavac: string, izdanje: string) {
-            this.idartikla = id;
-            this.naziv = naziv;
-            this.opis = opis;
-            this.dostupnakolicina = dostupnakolicina;
-            this.cijena = cijena;
-            this.pdv = pdv;
-            this.izdavac = izdavac;
-            this.izdanje = izdanje;
-        }
-
-        static async dohvatiSveArtikle() {
-            let artikli = await prisma.artikal.findMany();
-            return artikli;
-        }
-
-        static async dohvatiArtikal(id: number) {
-            let artikal = await prisma.artikal.findUnique({
-                where: {
-                    idartikla: id
-                }
-            });
-            return artikal;
-        }
-
-        
-
-    }
-
-export { artikalClass as Artikal };
-
-import {Artikal} from '../models/artikal';
-import { Request, Response } from 'express';
-
-export class ArtikalController{
-
-    static async apiDohvatiArtikal(req: Request, res: Response){
-        try {
-            let id = parseInt(req.params.id);
-            let artikal = await Artikal.dohvatiArtikal(id);
-            if (!artikal) {
-                res.status(400).json({error: "Artikal nije pronađen."});
-                return;
-            }
-            res.json(artikal);
-        }
-        catch (err) {
-            res.status(400).json({error: err});
-        }
-    }
-
-    static async apiDohvatiSveArtikle(req: Request, res: Response){
-        try {
-            let artikli = await Artikal.dohvatiSveArtikle();
-            res.json(artikli);
-        }
-        catch (err) {
-            res.status(400).json({error: err});
-        }
-    }
-
-}
-*/
-
-jest.mock('../../models/artikal', () => {
+jest.mock("../../models/artikal", () => {
     return {
         Artikal: {
             dohvatiSveArtikle: jest.fn().mockResolvedValue([
@@ -93,8 +13,8 @@ jest.mock('../../models/artikal', () => {
                     cijena: 100,
                     pdv: 25,
                     izdavac: "Izdavac 1",
-                    izdanje: "Izdanje 1"
-                }
+                    izdanje: "Izdanje 1",
+                },
             ]),
             dohvatiArtikal: jest.fn(async (id: number) => {
                 return {
@@ -105,17 +25,15 @@ jest.mock('../../models/artikal', () => {
                     cijena: 100,
                     pdv: 25,
                     izdavac: "Izdavac 1",
-                    izdanje: "Izdanje 1"
-                }
-            })
+                    izdanje: "Izdanje 1",
+                };
+            }),
+        },
+    };
+});
 
-        }
-    }
-}); 
-
-describe('Artikal controller integration tests', () => {
-   
-    it('apiDohvatiSveArtikle', async () => {
+describe("Artikal controller integration tests", () => {
+    it("apiDohvatiSveArtikle", async () => {
         let artikli = await Artikal.dohvatiSveArtikle();
         const req = {};
         const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
@@ -123,12 +41,11 @@ describe('Artikal controller integration tests', () => {
         expect(res.json).toHaveBeenCalledWith(artikli);
     });
 
-    it('apiDohvatiArtikal', async () => {
+    it("apiDohvatiArtikal", async () => {
         let artikal = await Artikal.dohvatiArtikal(1);
         const req = { params: { id: 1 } };
         const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
         await ArtikalController.apiDohvatiArtikal(req as any, res as any);
         expect(res.json).toHaveBeenCalledWith(artikal);
     });
-
 });
--- src/backend/__tests__/zaposlenikController.test.ts
+++ src/backend/__tests__/zaposlenikController.test.ts
@@ -3,49 +3,54 @@ import { ZaposlenikController } from "../controllers/zaposlenikController";
 
 jest.mock("../models/zaposlenik", () => ({
     Zaposlenik: {
-        dohvatiSveZaposlenike: jest.fn().mockResolvedValue([{ mbr: '1', ime: "Zaposlenik 1" }, { mbr: '2', ime: "Zaposlenik 2" }]),
+        dohvatiSveZaposlenike: jest.fn().mockResolvedValue([
+            { mbr: "1", ime: "Zaposlenik 1" },
+            { mbr: "2", ime: "Zaposlenik 2" },
+        ]),
         dohvatiZaposlenika: jest.fn(),
         dohvatiZaposlenikaPoEmailu: jest.fn(),
         dohvatiZaposlenikaPoOibu: jest.fn(),
-        dohvatiSveReferenteNabave: jest.fn().mockResolvedValue([{ mbr: '1', ime: "Zaposlenik 1" }, { mbr: '2', ime: "Zaposlenik 2" }]),
+        dohvatiSveReferenteNabave: jest.fn().mockResolvedValue([
+            { mbr: "1", ime: "Zaposlenik 1" },
+            { mbr: "2", ime: "Zaposlenik 2" },
+        ]),
         provjeriReferentaNabave: jest.fn(),
     },
 }));
 
-describe('Zaposlenik controller tests', () => {
-    it('apiDohvatiZaposlenika', async () => {
-        Zaposlenik.dohvatiZaposlenika = jest.fn().mockResolvedValueOnce({ mbr: '1', ime: "Zaposlenik 1" });
-        let req = { params: { mbr: '1' } };
+describe("Zaposlenik controller tests", () => {
+    it("apiDohvatiZaposlenika", async () => {
+        Zaposlenik.dohvatiZaposlenika = jest.fn().mockResolvedValueOnce({ mbr: "1", ime: "Zaposlenik 1" });
+        let req = { params: { mbr: "1" } };
         const res = { json: jest.fn() };
         await ZaposlenikController.apiDohvatiZaposlenika(req as any, res as any);
-        expect(res.json).toHaveBeenCalledWith({ mbr: '1', ime: "Zaposlenik 1" });
+        expect(res.json).toHaveBeenCalledWith({ mbr: "1", ime: "Zaposlenik 1" });
     });
 
-    it('apiDohvatiSveZaposlenike', async () => {
+    it("apiDohvatiSveZaposlenike", async () => {
         const req = {};
         const res = { json: jest.fn() };
         await ZaposlenikController.apiDohvatiSveZaposlenike(req as any, res as any);
         expect(res.json).toHaveBeenCalledWith([
-            { mbr: '1', ime: "Zaposlenik 1" },
-            { mbr: '2', ime: "Zaposlenik 2" },
+            { mbr: "1", ime: "Zaposlenik 1" },
+            { mbr: "2", ime: "Zaposlenik 2" },
         ]);
     });
 
-    it('apiDohvatiSveReferenteNabave', async () => {
+    it("apiDohvatiSveReferenteNabave", async () => {
         const req = {};
         const res = { json: jest.fn() };
         await ZaposlenikController.apiDohvatiSveReferenteNabave(req as any, res as any);
         expect(res.json).toHaveBeenCalledWith([
-            { mbr: '1', ime: "Zaposlenik 1" },
-            { mbr: '2', ime: "Zaposlenik 2" },
+            { mbr: "1", ime: "Zaposlenik 1" },
+            { mbr: "2", ime: "Zaposlenik 2" },
         ]);
     });
 
-    it('apiDohvatiSveIdReferentataNabave', async () => {
+    it("apiDohvatiSveIdReferentataNabave", async () => {
         const req = {};
         const res = { json: jest.fn() };
         await ZaposlenikController.apiDohvatiSveIdReferentataNabave(req as any, res as any);
-        expect(res.json).toHaveBeenCalledWith(['1', '2']);
+        expect(res.json).toHaveBeenCalledWith(["1", "2"]);
     });
-
 });
